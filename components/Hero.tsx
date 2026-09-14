56fd9e476118bad93d1b1b56e347693886c32423
Alonso Vera <alonsoveralarach@gmail.com>
2026-09-13 23:24:55 -0300

    hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,27 @@
+"use client";
 import Image from 'next/image';
+import CustomButton from './CustomButton';
 
 const Hero = () => {
+  const handleScroll = () => {
+
+  }
   return (
     <div className="hero">
       <div className="flex-1 pt-36 padding-x">
-        <h1></h1>
+        <h1 className="hero__title">
+          Free Cars!!
+        </h1>
+        <p className="hero__subtitle">
+          Many free cars for everyone.
+        </p>
+
+        <CustomButton 
+          title="Explore Cars"
+          containerStyles="bg-primary-blue
+          text-white rounded-full mt-10"
+          handleClick={handleScroll}
+        />
       </div>
     </div>
   )
