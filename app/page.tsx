e1f72113ce1c1ac62af2418f0d9be3d1ec441477
Alonso Vera <alonsoveralarach@gmail.com>
2026-09-14 21:52:05 -0300

    Rapid API cars

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-import { CustomFilter, Hero, SearchBar } from "@/components";
+import { CarCard, CustomFilter, Hero, SearchBar } from "@/components";
 import Image from "next/image";
+import { fetchCars } from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
   return (
       <main className="overflow-hidden">
         <Hero />
@@ -20,10 +23,22 @@ export default function Home() {
             <div className="home__filter-container">
               <CustomFilter title="fuel"/>
               <CustomFilter title="year"/>
-
             </div>
-
           </div>
+          {!isDataEmpty ? (
+            <section>
+              <div className="home__cars-wraper">
+                {allCars?.map((car) => 
+                <CarCard car={car} />)}
+              </div>
+            </section>
+          ): (
+            <div className="home__error-container">
+              <h2 className="text-black text-xl
+              font-bold">Oops, no results</h2>
+              <p>{allCars?.message}</p>
+            </div>
+          )}
 
         </div>
       </main>
--- components/index.ts
+++ components/index.ts
@@ -5,6 +5,8 @@ import Footer from "./Footer";
 import CustomFilter from "./CustomFilter";
 import SearchBar from "./SearchBar";
 import SearchManufacturer from "./SearchManufacturer";
+import CarCard from "./CarCard";
+import CardDetails from "./CardDetails";
 
 export {
     Hero,
@@ -13,5 +15,7 @@ export {
     Footer,
     CustomFilter,
     SearchBar,
-    SearchManufacturer
+    SearchManufacturer,
+    CarCard,
+    CardDetails
 }
